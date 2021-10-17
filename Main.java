860dc4971407264ed6ae8ae0cbfeb57df07f42a4
DESKTOP-QH0AMDR\Talgat <talgateasypack@gmail.com>
2021-10-17 15:36:24 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import com.dfs.ext1.App;
 public class Main {    
   
     public static void main(String[] args) throws Exception {
-
+        //mvn clean install -Denforcer.skip=true
         App.main(args);
         
     }
